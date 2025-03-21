79b7f6ce8284e07535c3acd0f12c095899d271a7
devsheke <abhi@sheke.tech>
2025-03-21 19:04:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<p>
+    <h1>Beamer (web)</h1>
+<p>
+
+<p>
+    <a aria-label="License" href="https://github.com/keryx-labs/beamer-web/blob/main/LICENSE"><img alt="" src="https://img.shields.io/github/license/keryx-labs/beamer-web?style=flat"></a>
+</p>
+
+This repository contains the web-based frontend of beamer that enables the users to manage and inspect instances of the Beamer bulk mailer.
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in
+      with pkgs; {
+        devShell = mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+            pnpm
+            prettierd
+            tailwindcss-language-server
+            typescript-language-server
+            vscode-langservers-extracted
+          ];
+        };
+      });
+}
